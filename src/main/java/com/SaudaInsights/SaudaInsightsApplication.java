0e3d3cac141f74e0126e3b3d359770736ca6c8ea
Rosalin Maharana <rosalin92.work@gmail.com>
2023-12-26 06:36:47 -0500

    Initial commit or Spring Boot setup
    Signed-off-by: Rosalin Maharana <rosalin92.work@gmail.com>

---
+++ src/main/java/com/SaudaInsights/SaudaInsightsApplication.java
@@ -0,0 +1,13 @@
+package com.SaudaInsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudaInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudaInsightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SaudaInsights/SaudaInsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SaudaInsights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudaInsightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
